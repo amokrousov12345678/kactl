c44d45f27d6c688fad2b0585a4571ff48fa1c5b9
Anton Mokrousov <settingx@mail.ru>
2023-11-08 17:18:43 +0700

    Add test for big indices in SegmentTreePersistent

--- stress-tests/data-structures/SegmentTreePersistent.cpp
+++ stress-tests/data-structures/SegmentTreePersistent.cpp
@@ -5,8 +5,10 @@
 int main() {
 	rep(n,1,10) {
 		vector<Node*> roots{new Node(n)};
+		vector<Node*> rootsBig{new Node(1'000'000'000)};
+		int SHIFT = 333'222'444;
 		vector<vi> v{vi(n, Node::unit)};
-		rep(it,0,1000000) {
+		rep(it,0,200000) {
 			int i = rand() % n, j = rand() % n;
 			int x = rand() % (n+2);
 
@@ -18,11 +20,14 @@ int main() {
 				rep(k,i,j+1) ma = max(ma, v[rev][k]);
 				auto expected = query(roots[rev], i,j);
 				assert(ma == expected);
+				auto cur = rootsBig[rev];
+				auto expectedBig = query(rootsBig[rev], i + SHIFT,j + SHIFT);
+				assert(ma == expectedBig);
 			}
 			else {
 				i = min(i, n-1);
-				auto newRoot = update(roots[rev], i, x);
-				roots.push_back(newRoot);
+				roots.push_back(update(roots[rev], i, x));
+				rootsBig.push_back(update(rootsBig[rev], i + SHIFT, x));
 				v.push_back(v[rev]);
 				v.back()[i] = x;
 			}
