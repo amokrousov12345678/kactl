a680f96dae1ffe5b362dfbe8cc9e2c657d7c6721
Anton Mokrousov <settingx@mail.ru>
2023-11-13 14:09:00 +0700

    Add python hasher to reference

---
+++ content/contest/hash.py
@@ -0,0 +1,12 @@
+# Hashes a file, ignoring all whitespace and comments. Use for
+# verifying that code was correctly typed.
+import hashlib, pathlib, re, sys
+if len(sys.argv) >= 2:
+    data_b = pathlib.Path(sys.argv[1]).read_bytes()
+else:
+    data_b = sys.stdin.read().encode('utf-8')
+data = data_b.decode('ascii')
+data = re.sub('//.*?\n|/\*.*?\*/', '', data, flags=re.S)
+data = ''.join(data.split()).encode('utf-8')
+print(hashlib.md5(data).hexdigest()[0:6])
+if len(sys.argv) >= 2: input()
